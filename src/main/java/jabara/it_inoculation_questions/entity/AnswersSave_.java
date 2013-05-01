1d1ae2d9912469e912b3865fcd4aa3f59a86da90
Jabara <jabaraster@gmail.com>
2013-05-01 19:42:30 +0900

    mainte.

--- src/main/java/jabara/it_inoculation_questions/entity/AnswersSave_.java
+++ src/main/java/jabara/it_inoculation_questions/entity/AnswersSave_.java
@@ -1,14 +1,13 @@
 package jabara.it_inoculation_questions.entity;
 
-import jabara.jpa.entity.EntityBase_;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-02-15T15:05:59.317+0900")
+@Generated(value="Dali", date="2013-05-01T08:11:31.865+0900")
 @StaticMetamodel(AnswersSave.class)
-public class AnswersSave_ extends EntityBase_ {
+public class AnswersSave_ {
 	public static volatile SingularAttribute<AnswersSave, String> key;
 	public static volatile ListAttribute<AnswersSave, Answer> answers;
 }
--- src/main/java/jabara/it_inoculation_questions/entity/Answers_.java
+++ src/main/java/jabara/it_inoculation_questions/entity/Answers_.java
@@ -1,12 +1,11 @@
 package jabara.it_inoculation_questions.entity;
 
-import jabara.jpa.entity.EntityBase_;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-02-15T13:58:28.191+0900")
+@Generated(value="Dali", date="2013-05-01T08:11:31.850+0900")
 @StaticMetamodel(Answers.class)
-public class Answers_ extends EntityBase_ {
+public class Answers_ {
 	public static volatile ListAttribute<Answers, Answer> answers;
 }
